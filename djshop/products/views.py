478e28ff9d56acd838757243a90383a958975ddc
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2022-04-30 18:24:34 +0430

    7th,BASE_DIR /'templates',path('path/',include('app_name.urls'),the changes when start the project,use base.html and other in file,{% extends 'path' %},block

--- djshop/products/views.py
+++ djshop/products/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
+from .models import products
 # Create your views here.
 
 
+def products_show(request):
+    prod_dict ={
+        'all_products':products.objects.all(),
+    }
+    return render(request,'products/products_list.html',prod_dict)
 
 
-# lkj
-# sldkfj
-# lskdj
-# lsdkjfjfd
+
+
+    
