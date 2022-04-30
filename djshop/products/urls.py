456ab11b914df14134a44be6f35ed4ffeae31bc4
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2022-04-30 23:09:47 +0430

    8th.import get or 404,try,except,raise Http404()

--- djshop/products/urls.py
+++ djshop/products/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('all',views.products_show),
+    path('<int:prod_id>',views.product_detail)
 ]
--- djshop/products/views.py
+++ djshop/products/views.py
@@ -9,6 +9,20 @@ def products_show(request):
     }
     return render(request,'products/products_list.html',prod_dict)
 
+from django.http import Http404
+def product_detail(request,prod_id):
+    '''
+    try:
+        prods=products.objects.get(id=prod_id)
+    except:
+        raise Http404("this isn't found")
+    ''' 
+    from django.shortcuts import get_object_or_404
+    prods=get_object_or_404(products,id=prod_id)
+    detail_dict = {
+        'a_product':prods,
+    }
+    return render(request,'products/item_detail.html',detail_dict)
 
 
 
