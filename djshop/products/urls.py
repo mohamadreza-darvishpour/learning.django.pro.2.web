95731f4cf899c8d8f117229eed1f30b161e34e5d
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2022-05-01 19:43:39 +0430

    9th.send urls by'{%url  'url_name' arg1=val1 ,....%}' by give name to urls.

--- djshop/products/urls.py
+++ djshop/products/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 
 urlpatterns = [
-    path('all',views.products_show),
-    path('<int:prod_id>',views.product_detail)
+    path('all',views.products_show , name='all_prods'),
+    path('<int:prod_id>',views.product_detail , name= 'prod_detail'),
 ]
