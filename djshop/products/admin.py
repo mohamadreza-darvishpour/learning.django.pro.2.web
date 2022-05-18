221af0410375e90e0a9023e3145c35f67d67dd45
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2022-05-19 02:47:26 +0430

    26th.some changes in proj

--- djshop/products/admin.py
+++ djshop/products/admin.py
@@ -3,17 +3,13 @@ from django.contrib import admin
 # Register your models here.
 from . import models
 
-class product_admin(admin.ModelAdmin):
-#    readonly_fields= ['slug','is_active']
-    prepopulated_fields = {
-        'slug':['title'],
-    }
-    list_display = ('id','short_description','price','slug')
-    list_filter = ['price']
-    list_editable = ['price',]
+class productadmin(admin.ModelAdmin):
+    list_display = ['id','title','price','slug','short_description','is_active','is_deleted']
+    list_filter = ('price','is_active')
+    list_editable = ['title','price','is_active' ]
 
 
-admin.site.register(models.products,product_admin)
+admin.site.register(models.products,productadmin)
 admin.site.register(models.product_category)
 admin.site.register(models.product_info)
 admin.site.register(models.product_tag)
