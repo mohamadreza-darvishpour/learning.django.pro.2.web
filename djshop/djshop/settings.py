a207655eb63a0310d61ddff4ede8ec8184db2a5d
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2022-05-20 13:22:29 +0430

    27th,use ready templates of frontend to make a websites.change the wrong path in those files.in the 61th vid.

--- djshop/djshop/settings.py
+++ djshop/djshop/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products',
+    'home_module',
 ]
 
 MIDDLEWARE = [
@@ -117,7 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+#new line changed below
+STATICFILES_DIRS=[
+    BASE_DIR / 'static',
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- djshop/djshop/urls.py
+++ djshop/djshop/urls.py
@@ -18,5 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("home/",include('home_module.urls')),
     path('shop/',include('products.urls')),
 ]
