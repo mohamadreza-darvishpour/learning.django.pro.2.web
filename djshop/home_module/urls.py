441a293f3a25210be0e7fb663b223b50c5736442
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2022-05-21 19:46:20 +0430

    28th,render_partial usage and download,include,extends,dynamic site by partial and more abbrevite,render_partial by url and without it.

--- djshop/home_module/urls.py
+++ djshop/home_module/urls.py
@@ -4,6 +4,10 @@ from . import views
 
 
 urlpatterns = [
+    path("",views.layout),
     path("inhome",views.index_page),
-
+    #path("contact_us",views.contact_us),
+    path("footer_p",views.footer_partial,name="footer_pa"),
+    path("header_p",views.header_partial,name="header_pa"),
+ 
 ]
--- djshop/home_module/views.py
+++ djshop/home_module/views.py
@@ -4,3 +4,16 @@ from django.shortcuts import render
 
 def index_page(request):
     return render(request,'home_module/index_page1.html')
+
+
+def header_partial(request):
+    return render(request,"shared/header_partial.html")
+
+def footer_partial(request):
+    return render(request,"shared/footer_partial.html")
+
+def contact_us(request):
+    return render(request,"contact_us")
+
+def layout(request):
+    return render(request,"_layout.html")
