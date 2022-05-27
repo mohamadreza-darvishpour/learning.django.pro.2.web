db68ee3689c2e24c2d20cf9102b33dcd5cab42d7
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2022-05-27 21:18:14 +0430

    33rd,use forms,get,post,redirect,reverse,request.method,csrf-token,action

--- djshop/contact/urls.py
+++ djshop/contact/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path("contact_us",views.contact_us),
+    path("contact_us",views.contact_us,name="contact_page"),
 ]
--- djshop/contact/views.py
+++ djshop/contact/views.py
@@ -5,4 +5,6 @@ from django.shortcuts import render
 
 
 def contact_us(request):
+    if request.method =='POST':
+        print(request.POST)
     return render(request,"contact/contact_us.html",{})
