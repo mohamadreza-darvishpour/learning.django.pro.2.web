9f330d5437f00d52b8e20ba2e237a8116ed7b4ba
mohamadreza-darvishpour <mohamadrezalrd@yahoo.com>
2022-05-31 01:29:02 +0430

    35th,label,required,widget,TextArea,...

--- djshop/contact/forms.py
+++ djshop/contact/forms.py
@@ -3,6 +3,12 @@ from django  import forms
 
 class contact_us_form(forms.Form):
     full_name = forms.CharField(label="first-last-family")
-    email = forms.EmailField()
-    subject = forms.CharField()
-    text = forms.CharField()
+    email = forms.EmailField(label="email",widget=forms.EmailInput)
+    subject = forms.CharField(label="subj-",
+    max_length =50,
+    required = True,
+    error_messages={
+        'required':"enter subject."
+    },
+    )
+    text = forms.CharField(label="the text",widget=forms.TextInput)
--- djshop/contact/views.py
+++ djshop/contact/views.py
@@ -6,12 +6,15 @@ from .forms import contact_us_form
 
 
 def contact_us(request):
+    #        contact_form = contact_us_form(request.POST or None)
+
     if request.method == 'POST':
         contact_form = contact_us_form(request.POST)
-        contact_form.is_valid()  
-        print("*********\n",contact_form.cleaned_data ,"\n*******************")
-        return redirect("all_prods")
-    contact_form = contact_us_form(request.POST)
+        if contact_form.is_valid():
+            print("*********\n",contact_form.cleaned_data ,"\n*******************")
+            return redirect("all_prods")
+    else:
+        contact_form = contact_us_form()
     contact_form_dict = {
         "contact_form":contact_form,
     }
